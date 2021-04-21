d3c4b9b85c838a833a89ff814915dbc3911ed177
FloydATC <floyd@atc.no>
2021-04-21 20:55:32 +0200

    Can now render a light blue background

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use amethyst::utils::application_root_dir;
 use amethyst::SimpleState;
 use amethyst::GameDataBuilder;
 use amethyst::Application;
+use amethyst::renderer::plugins::RenderPbr3D;
+use amethyst::renderer::plugins::RenderToWindow;
+use amethyst::renderer::types::DefaultBackend;
+use amethyst::renderer::RenderingBundle;
+use amethyst::window::DisplayConfig;
 
 
 struct GameState;
@@ -20,8 +25,23 @@ fn main() -> amethyst::Result<()> {
     let app_root = application_root_dir()?;
     let assets_dir = app_root.join("assets");
 
+    // Set up the display configuration
+    let display_config = DisplayConfig {
+        title: "Hello Amethyst".to_string(),
+        dimensions: Some((640, 400)),
+        ..Default::default()
+    };
+
     // Set up an empty (for now) GameDataBuilder
-    let game_data = GameDataBuilder::default();
+    let game_data = GameDataBuilder::default()
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new() // "vulkan" selected in Cargo.toml
+            .with_plugin(
+                RenderToWindow::from_config(display_config)
+                    .with_clear([0.529, 0.808, 0.98, 1.0]), // [R,G,B,A]
+            )
+            .with_plugin(RenderPbr3D::default()), // Physically Based Rendering
+    )?;
 
     // Run the game!
     let mut game = Application::new(assets_dir, GameState, game_data)?;
