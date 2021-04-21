53307c42daec2f55fa64af7f3e2249124e00e749
FloydATC <floyd@atc.no>
2021-04-21 22:18:50 +0200

    Error: Cannot find type "Mesh" in this scope

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,28 @@ use amethyst::renderer::plugins::RenderPbr3D;
 use amethyst::renderer::plugins::RenderToWindow;
 use amethyst::renderer::types::DefaultBackend;
 use amethyst::renderer::RenderingBundle;
+use amethyst::renderer::shape::Shape;
+use amethyst::renderer::Camera;
+use amethyst::renderer::Material;
+use amethyst::renderer::MaterialDefaults;
+use amethyst::renderer::rendy::mesh::{Normal, Position, Tangent, TexCoord};
 use amethyst::window::DisplayConfig;
+use amethyst::StateData;
+use amethyst::GameData;
+use amethyst::prelude::World;
+use amethyst::prelude::WorldExt;
+use amethyst::prelude::Builder;
+use amethyst::assets::AssetLoaderSystemData;
+use amethyst::core::{Transform, TransformBundle};
 
 
 struct GameState;
-impl SimpleState for GameState {}
+impl SimpleState for GameState {
+    fn on_start(&mut self, state_data: StateData<'_, GameData<'_, '_>>) {
+        initialize_camera(state_data.world);
+        initialize_sphere(state_data.world);
+    }
+}
 
 
 fn main() -> amethyst::Result<()> {
@@ -34,6 +51,7 @@ fn main() -> amethyst::Result<()> {
 
     // Set up an empty (for now) GameDataBuilder
     let game_data = GameDataBuilder::default()
+    .with_bundle(TransformBundle::new())?
     .with_bundle(
         RenderingBundle::<DefaultBackend>::new() // "vulkan" selected in Cargo.toml
             .with_plugin(
@@ -48,4 +66,47 @@ fn main() -> amethyst::Result<()> {
     game.run();
 
     Ok(())
+}
+
+
+fn initialize_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(0.0, 0.0, 10.0);
+
+    world.create_entity()
+        .with(Camera::standard_3d(1024.0, 768.0))
+        .with(transform)
+        .build();
+}
+
+
+fn initialize_sphere(world: &mut World) {
+    let mesh = world.exec(|loader: AssetLoaderSystemData<'_, Mesh>| {
+        loader.load_from_data(
+            Shape::Sphere(100, 100)
+                .generate::<(Vec<Position>, Vec<Normal>, Vec<Tangent>, Vec<TexCoord>)>(None)
+                .into(),
+            (),
+        )
+    });
+
+    let material_defaults = world.read_resource::<MaterialDefaults>().0.clone();
+    let material = world.exec(|loader: AssetLoaderSystemData<'_, Material>| {
+        loader.load_from_data(
+                Material {
+                    ..material_defaults
+                },
+                (),
+            )
+        },
+    );
+
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(0.0, 0.0, 0.0);
+
+    world.create_entity()
+        .with(mesh)
+        .with(material)
+        .with(transform)
+        .build();
 }
